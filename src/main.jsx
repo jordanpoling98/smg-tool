0236308e5d2b8ec6ab91886179d1738bbdc83109
jordanpoling98 <jordan.poling100@gmail.com>
2025-04-08 22:58:55 -0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PromoEmailCopier from './PromoEmailCopier'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PromoEmailCopier />
+  </React.StrictMode>,
+)
