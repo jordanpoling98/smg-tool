6d0426626554d5011587c02fa6545ac7e16f854a
jordanpoling98 <jordan.poling100@gmail.com>
2025-04-08 23:03:29 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import PromoEmailCopier from './PromoEmailCopier'
+import PromoEmailCopier from './PromoEmailCopier.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
